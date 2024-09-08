0fda7b29105b655aa9c6abf7414315eb3d02c1d0
dariamakhno <makhnodaria@gmail.com>
2024-09-08 18:50:57 +0300

    dz

---
+++ 5.3.py
@@ -0,0 +1,10 @@
+punctuation_symbols = r''' !"#$%&'()*+,-./:;<=>?@[\]_^`{|}~'''
+punctuation_symbols_list = [i for i in punctuation_symbols]
+
+user_variable = input('Input your sentence: ').title()
+hashtag = "#" + ''.join([i for i in user_variable if i not in punctuation_symbols_list])
+
+if len(hashtag) > 140:
+    hashtag = hashtag[0:140]
+
+print(f'The hashtag is created: {hashtag}')
